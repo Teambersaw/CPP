31f608d19c4cd4dcf52cc6758f911d10c6c70e44
jrossett <jules.rossetti@aol.fr>
2022-10-27 18:39:59 +0200

    ex00

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -5,15 +5,30 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jrossett <jrossett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/27 14:18:57 by jrossett          #+#    #+#             */
-/*   Updated: 2022/10/27 15:23:14 by jrossett         ###   ########.fr       */
+/*   Created: 2022/10/27 18:05:54 by jrossett          #+#    #+#             */
+/*   Updated: 2022/10/27 18:31:33 by jrossett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "iostream"
+#include <iostream>
 
-int main()
+int main(int ac, char **av)
 {
-	std::cin
+	int i = 0;
+	int j;
+
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE NOISE *" << std::endl;
+	else
+	{
+		while (av[++i])
+		{
+			j = 0;
+			while (av[i][j])
+				std::cout << (char)(std::toupper(av[i][j++]));
+
+		}
+		std::cout << std::endl;
+	}
 	return (0);
 }
