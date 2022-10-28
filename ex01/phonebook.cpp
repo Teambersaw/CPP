3159fd8adbd1664ad7dee7809d008e92a031569a
jrossett <jules.rossetti@aol.fr>
2022-10-28 21:08:00 +0200

    ex01 done

--- ex01/phonebook.cpp
+++ ex01/phonebook.cpp
@@ -6,14 +6,209 @@
 /*   By: jrossett <jrossett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/27 18:53:38 by jrossett          #+#    #+#             */
-/*   Updated: 2022/10/27 19:55:55 by jrossett         ###   ########.fr       */
+/*   Updated: 2022/10/28 21:00:05 by jrossett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
 #include "phonebook.hpp"
 
-int main()
+PhoneBook::PhoneBook(void) : _nb_contact(0), _max_contact(0) {
+	return ;
+}
+
+PhoneBook::~PhoneBook(void) {
+	std::cout << "See you soon !!" << std::endl;
+	return ;
+}
+
+Contact::Contact(void) {
+	return ;
+}
+
+Contact::~Contact(void) {
+	return ;
+}
+
+void Contact::set_first_name() {
+	while (1)
+	{
+		std::cout << "First name: ";
+		std::getline(std::cin, _first_name);
+		if (std::cin.eof()) {
+			std::cout << std::endl << "See you soon !!" << std::endl;
+			std::exit(0);
+		}
+		if (_first_name.empty() == false)
+			return ;
+	}
+}
+
+void Contact::set_last_name() {
+	while (1)
+	{
+		std::cout << "Last name: ";
+		std::getline(std::cin, _last_name);
+		if (std::cin.eof()) {
+			std::cout << std::endl << "See you soon !!" << std::endl;
+			std::exit(0);
+		}
+		if (_last_name.empty() == false)
+			return ;
+	}
+}
+
+void Contact::set_nickname() {
+	while (1)
+	{
+		std::cout << "Nickname: ";
+		std::getline(std::cin, _nickname);
+		if (std::cin.eof()) {
+			std::cout << std::endl << "See you soon !!" << std::endl;
+			std::exit(0);
+		}
+		if (_nickname.empty() == false)
+			return ;
+	}
+}
+
+void Contact::set_phone_number() {
+	while (1)
+	{
+		std::cout << "Phone number: ";
+		std::getline(std::cin, _phone_number);
+		if (std::cin.eof()) {
+			std::cout << std::endl << "See you soon !!" << std::endl;
+			std::exit(0);
+		}
+		if (_phone_number.empty() == false)
+			return ;
+	}
+}
+
+void Contact::set_darkest_secret() {
+	while (1)
+	{
+		std::cout << "Darkest secret: ";
+		std::getline(std::cin, _darkest_secret);
+		if (std::cin.eof()) {
+			std::cout << std::endl << "See you soon !!" << std::endl;
+			std::exit(0);
+		}
+		if (_darkest_secret.empty() == false)
+			return ;
+	}
+}
+
+void Contact::get_contact(int index) {
+
+	std::string str;
+
+	if (_first_name.empty()) {
+		return ;
+	}
+	std::cout << "|" << std::setw(10) << index;
+	str = _first_name;
+	if (str.length() > 10)
+	{
+		str.replace(9, str.length() - 9, ".");
+	}
+	std::cout << "|" << std::setw(10) << str;
+	str = _last_name;
+	if (str.length() > 10)
+	{
+		str.replace(9, str.length() - 9, ".");
+	}
+	std::cout << "|" << std::setw(10) << str;
+	str = _nickname;
+	if (str.length() > 10)
+	{
+		str.replace(9, str.length() - 9, ".");
+	}
+	std::cout << "|" << std::setw(10) << str;
+	std::cout << "|" << std::endl;
+	return ;
+}
+
+void Contact::aff_contact(void) {
+	std::cout << "First name: " << _first_name << std::endl;
+	std::cout << "Last name: " << _last_name << std::endl;
+	std::cout << "Nickname: " << _nickname << std::endl;
+	std::cout << "Phone number: " << _phone_number << std::endl;
+	std::cout << "Darkest secret: " << _darkest_secret << std::endl;
+}
+
+void PhoneBook::add(void) {
+	if (_nb_contact > 7)
+		_nb_contact = 0;
+	std::cout << "PhoneBook need some information to create your new contact:" << std::endl;
+	_contact[_nb_contact].set_first_name();
+	_contact[_nb_contact].set_last_name();
+	_contact[_nb_contact].set_nickname();
+	_contact[_nb_contact].set_phone_number();
+	_contact[_nb_contact].set_darkest_secret();
+	_nb_contact++;
+	_max_contact++;
+}
+
+void PhoneBook::search(void) {
+	int index = 0;
+	std::string c;
+	std::cout << "Contacts:" << std::endl;
+	std::cout << "|     Index|First name| Last name|  Nickname|" << std::endl;
+	if (_nb_contact == 0)
+	{
+		std::cout << "You dont have any contact, you can create one with the command ADD !" << std::endl;
+		return ;
+	}
+	while (index < 8)
+	{
+		_contact[index].get_contact(index + 1);
+		index++;
+	}
+	while (1)
+	{
+		std::cout << "Index of the contact: ";
+		std::getline(std::cin, c);
+		if (std::cin.eof()) {
+			std::cout << std::endl << "See you soon !!" << std::endl;
+			std::exit(0);
+		}
+		if (c.empty() == false)
+		{
+			index = atoi(c.c_str());
+			if (c.length() == 1 && (index >= 1 && index <= 8))
+			{
+				if (index > _max_contact)
+					std::cout << "This contact index doesn't exist !" << std::endl;
+				else
+					break;
+			}
+			else
+				std::cout << "Please enter a valid index between 1 and 8" << std::endl;
+		}
+	}
+	this->_contact[index - 1].aff_contact();
+}
+
+int main(void)
 {
+	PhoneBook	phonebook;
+	std::string str;
+
+	std::cout << "Welcome in your awesome PhoneBook, you can use the commmand below:\nADD: to create a new contact\nSEARCH: to see your contacts\nEXIT: to leave your PhoneBook (destroy contacts)" << std::endl;
+	while (1)
+	{
+		std::cout << "PhoneBook: ";
+		std::getline(std::cin, str);
+		if (std::cin.eof()) {
+			return (0);
+		}
+		if (str.compare("ADD") == 0)
+			phonebook.add();
+		else if (str.compare("SEARCH") == 0)
+			phonebook.search();
+		else if (str.compare("EXIT") == 0)
+			return (0);
+	}
 	return (0);
 }
--- ex01/phonebook.hpp
+++ ex01/phonebook.hpp
@@ -6,11 +6,55 @@
 /*   By: jrossett <jrossett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/27 18:53:40 by jrossett          #+#    #+#             */
-/*   Updated: 2022/10/28 00:00:00 by jrossett         ###   ########.fr       */
+/*   Updated: 2022/10/28 20:07:23 by jrossett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHONEBOOK_HPP
 # define PHONEBOOK_HPP
+
+#include <iomanip>
+#include <iostream>
+
+class Contact {
+
+	public:
+
+		Contact(void);
+		~Contact(void);
+
+		void set_first_name(void);
+		void set_last_name(void);
+		void set_nickname(void);
+		void set_phone_number(void);
+		void set_darkest_secret(void);
+		void get_contact(int index);
+		void aff_contact(void);
+
+	private:
+		std::string _first_name;
+		std::string _last_name;
+		std::string _nickname;
+		std::string _phone_number;
+		std::string _darkest_secret;
+
+};
+
+class PhoneBook {
+
+	public:
+
+		PhoneBook(void);
+		~PhoneBook(void);
+
+		void add(void);
+		void search(void);
+
+	private:
+
+		int		_max_contact;
+		int		_nb_contact;
+		Contact _contact[8];
+};
 
 #endif
