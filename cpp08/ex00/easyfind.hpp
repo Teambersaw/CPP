5b9c8834685b03094e00c95ff07647f8eb6fd699
Jules ROSSETTI <jrossett@e1r4p21.clusters.42paris.fr>
2022-12-08 17:09:21 +0100

    cpp08 started

---
+++ cpp08/ex00/easyfind.hpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jrossett <jrossett@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/08 09:41:42 by jrossett          #+#    #+#             */
+/*   Updated: 2022/12/08 14:07:55 by jrossett         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+template< typename T >
+void easyfind(T contain, int occur) {
+	return ;
+}
+
+#endif
