9a480c43ca148eb9e141bcc13cac67cdf50bb7a8
Jules ROSSETTI <jrossett@e1r4p21.clusters.42paris.fr>
2022-12-09 16:28:25 +0100

    cpp08 ex00 started

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -6,16 +6,26 @@
 /*   By: jrossett <jrossett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 09:41:42 by jrossett          #+#    #+#             */
-/*   Updated: 2022/12/08 14:07:55 by jrossett         ###   ########.fr       */
+/*   Updated: 2022/12/09 14:09:49 by jrossett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EASYFIND_HPP
 # define EASYFIND_HPP
 
+# include <algorithm>
+# include <iostream>
+# include <vector>
+# include <iterator>
+# include <stdexcept>
+
 template< typename T >
-void easyfind(T contain, int occur) {
-	return ;
+int easyfind(T contain, int occur) {
+	typename T::iterator	it;
+	it = std::find(contain.begin(), contain.end(), occur);
+	if (it == contain.end())
+		throw (std::exception());
+	return (*it);
 }
 
 #endif
--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -6,11 +6,22 @@
 /*   By: jrossett <jrossett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 09:41:30 by jrossett          #+#    #+#             */
-/*   Updated: 2022/12/08 09:42:38 by jrossett         ###   ########.fr       */
+/*   Updated: 2022/12/09 14:17:29 by jrossett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "easyfind.hpp"
+
 int main()
 {
+	std::vector<int> myvector(30, 0);
+	std::queue<int> myvector(30, 0);
+	myvector[5] = 1;
+	try {
+		std::cout << easyfind<std::vector<int> >(myvector, 1) << std::endl;
+	}
+	catch(std::exception const &e) {
+		std::cerr << e.what() << std::endl;
+	}
 	return (0);
 }
